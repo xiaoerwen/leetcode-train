93e1b1293046d0fb03ee70f2d48e5dc0bdf4bf9d
caixiaowen <caixiaowen@baidu.com>
2021-03-20 22:23:56 +0800

    20210320打卡

--- medium/#150/solution.js
+++ medium/#150/solution.js
@@ -1,41 +1,47 @@
 /**
- * @file 翻转字符串里的单词
+ * @file 逆波兰式
  * @author caixiaowen
  */
 
 /**
- * 运行时间84ms，内存消耗39.9MB
- * 
- * 思路：双向队列
+ * 思路：表达式相关的计算都可以用栈，利用栈存储前一次的结果
+ * @param {*} tokens 
  */
-const reverseWords = function(s) {
-    s = s.trim();
-
-    if (!s.length) {
-        return '';
+const evalRPN = function(tokens) {
+    let stack = [];
+    let result;
+    for (let i = 0, len = tokens.length; i < len; i++) {
+        if (~'+-*/'.indexOf(tokens[i])) {
+            // 表达式右边的值出栈
+            let right = +stack.pop();
+            // 表达式左边的值出栈
+            let left = +stack.pop();
+            switch(tokens[i]) {
+                case '+':
+                    result = left + right;
+                    break;
+                case '-':
+                    result = left - right;
+                    break;
+                case '*':
+                    result = left * right;
+                    break;
+                case '/':
+                    // 除法取整数
+                    result = parseInt(left / right);
+                    break;
+            }
+            // 计算结果入栈，供下一次计算使用
+            stack.push(result);
+        }
+        else {
+            stack.push(tokens[i]);
+        }
     }
 
-    let word = '';
-    let queue = [];
-    for (let i = 0, len = s.length; i < len; i++) {
-        if (s[i] !== ' ') {
-            word+= s[i];
-        }
-        else if (word.length){
-            queue.unshift(word);
-            word = '';
-        }
-    }
-
-    word.length && queue.unshift(word);
-
-    return queue.join(' ');
+    // 这里注意结果已经入栈，所以还是从栈拿，避免出现result为undefined的情况
+    return stack.pop();
 };
 
-/**
- * 很牛逼的一行代码搞定
- * 运行时间68ms，内存消耗39.3MB
- * 
- * 思路：正则+数组翻转
- */
-const reverseWords = s => s.match(/\S+/g).reverse().join(' ');
+const tokens = ["4","13","5","/","+"];
+console.log(evalRPN(tokens));
