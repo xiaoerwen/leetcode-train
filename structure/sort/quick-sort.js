b464665890cfdc2b71713abf29a0a89568cb2f98
caixiaowen <caixiaowen@baidu.com>
2021-04-08 19:34:58 +0800

    20210407打卡

--- structure/sort/quick-sort.js
+++ structure/sort/quick-sort.js
@@ -3,11 +3,11 @@
  * @author xiaoerwen
  */
 
-const wrap = (arr, i, j) => {
-    let tmp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = tmp;
-}
+const swap = (items, i, j) => {
+    let tmp = items[i];
+    items[i] = items[j];
+    items[j] = tmp;
+};
 
 /**
  * 思路：以中位数为基准，双指针遍历数组
@@ -28,25 +28,31 @@ const quickSort = nums => {
     let base = nums[Math.floor(len / 2)];
     let i = 0;
     let j = len - 1;
-    while (i < j) {
-        while (nums[i] < base) {
+
+    while (i <= j) {
+        while (nums[i] < base && i < len) {
             i++;
         }
-        while (nums[j] > base) {
+        while (nums[j] > base && j >= 0) {
             j--;
         }
         if (i < j) {
-            wrap(nums, i, j);
+            swap(nums, i, j);
             i++;
             j--;
         }
+        else {
+            break;
+        }
     }
+    
+    return quickSort(nums.slice(0, i)).concat(quickSort(nums.slice(i)));
+    
 
-    // 注意指针边界，否则会报错
-    let left = i >= 0 ? nums.slice(0, i) : [];
-    let right = i > len ? [] : nums.slice(i);
-    return quickSort(left).concat(quickSort(right));
 };
 
-const nums = [8,4,9,3,6,2,0,7,1,5];
+let nums = [5,1,6,2,5];
+console.log(quickSort(nums));
+
+nums = [4,6,7,2,1,3,2,1,9,0];
 console.log(quickSort(nums));
